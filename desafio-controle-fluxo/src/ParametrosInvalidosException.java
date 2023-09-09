177809e411cf36fb013142be2c920de12af1cf55
Carol Fagundes <carolinarftk@gmail.com>
2023-09-09 20:23:20 -0300

    adicionando desafio controle de fluxo

---
+++ desafio-controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+
+public class ParametrosInvalidosException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+	}
+}
