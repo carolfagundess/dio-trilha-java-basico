f19e97fb8039c2bbcc78403fe30df861872d82d1
Carol Fagundes <carolinarftk@gmail.com>
2023-09-10 02:17:01 -0300

    enviano desafio java básico condicionais

---
+++ desafio-condicionalmente-rico/src/application/CondicionalmenteRico.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class CondicionalmenteRico {
+	public static void main(String[] args) {
+
+	    // Entrada de dados
+	    Scanner scanner = new Scanner(System.in);
+	    int saldoTotal = scanner.nextInt();
+	    int valorSaque = scanner.nextInt();
+
+	    //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+	    if (saldoTotal>valorSaque){
+	      System.out.println("Saque realizado com sucesso!");
+	      saldoTotal = saldoTotal - valorSaque;
+	      System.out.println("Novo saldo: " + saldoTotal);
+	    } else{
+	      System.out.println("Saldo insuficiente. Saque nao realizado!");
+	    }
+	    scanner.close();
+	  }
+}
